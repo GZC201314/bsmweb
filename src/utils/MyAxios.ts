b3f6fa253ee095c2fc19fa2d0984a7f6ddfd965a
GZC201314 <1739084007@qq.com>
2022-09-18 17:31:22 +0800

    解决登录过期提示问题

--- src/utils/MyAxios.ts
+++ src/utils/MyAxios.ts
@@ -4,13 +4,14 @@ import {message} from "antd";
 
 axios.interceptors.response.use(function (response) {
     // 登录权限验证失败
-    if (response.status && response.status == 302) {
+    if (response.status && response.status === 302) {
         window.history.back();
         message.error("登录信息已过期,请重新登录。")
     } else {
         return response;
     }
 }, function (error) {
+    message.error("登录信息已过期,请重新登录。")
     return Promise.reject(error);
 })
 
