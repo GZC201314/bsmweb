e3e8b3ee5c9b17aa1bbc8509984a3e6d2ffe4bb8
GZC201314 <1739084007@qq.com>
2022-12-15 19:01:54 +0800

    新增系统监控websocket

--- src/views/mainContent/home/index.tsx
+++ src/views/mainContent/home/index.tsx
@@ -1,10 +1,11 @@
-import {ArrowDownOutlined, ArrowUpOutlined } from '@ant-design/icons';
-import {Card, Col, Divider, message, Row, Statistic} from 'antd';
-import React, {FC, useEffect, useState } from 'react'
+import {Card, Col, Divider, Row, Statistic} from 'antd';
+import React, {FC, useEffect, useState} from 'react'
 import './style.scss'
 import _ from "lodash";
-import homeDao from "../../../dao/homeDao";
+import * as echarts from 'echarts';
 import {useHistory} from "react-router-dom";
+import {EChartOption} from "echarts";
+
 export interface HomeProps{
 
 }
@@ -17,81 +18,174 @@ const Home:FC<HomeProps> = (props) => {
     /**effect  effect部分**/
 
     useEffect(()=>{
-        homeDao.getSystemDetailInfo({},(res:any) =>{
-            if(res.code === 200){
-                if(_.isEqual(data,res.data)){
-                    return;
-                }
-                setData(res.data);
-                return;
-            }
-            // message.error("登录信息已过期,请重新登录。")
-            // history.push({
-            //     pathname: '/login'
-            // });
-        },(res:any) =>{
-            console.log(res)
-        })
-
+        getSysTemDetailInfo();
     },[])
     /**methods 方法部分**/
 
-    // const getSysTemDetailInfo = () =>{
-    //     debugger
-    //
-    //
-    //     //判断当前浏览器是否支持WebSocket, 主要此处要更换为自己的地址
-    //     if ('WebSocket' in window) {
-    //         websocket = new WebSocket("ws://localhost:8888/system/monitor");
-    //     } else {
-    //         alert('Not support websocket')
-    //     }
-    //
-    //     //连接发生错误的回调方法
-    //     websocket.onerror = function() {
-    //         console.log("error")
-    //     };
-    //
-    //     //连接成功建立的回调方法
-    //     websocket.onopen = function(event) {
-    //         console.log("open")
-    //         //setMessageInnerHTML("open");
-    //     }
-    //
-    //     //接收到消息的回调方法
-    //     websocket.onmessage = function(event) {
-    //         debugger
-    //         const parse = JSON.parse(event.data);
-    //         if(_.isEqual(data,parse)){
-    //            return;
-    //         }
-    //         setData(parse)
-    //     }
-    //
-    //     //连接关闭的回调方法
-    //     websocket.onclose = function() {
-    //         console.log("close");
-    //     }
-    //
-    //     //监听窗口关闭事件，当窗口关闭时，主动去关闭websocket连接，防止连接还没断开就关闭窗口，server端会抛异常。
-    //     window.onbeforeunload = function() {
-    //         websocket.close();
-    //     }
-    //
-    //
-    //
-    //     //关闭连接
-    //     function closeWebSocket() {
-    //         if(!_.isEmpty(websocket)){
-    //             websocket.close();
-    //         }
-    //     }
-    //
-    //     //发送消息
-    //     function send() {
-    //         // websocket.send(message);
-    //     }
-    // }
+    const getSysTemDetailInfo = () =>{
+        //判断当前浏览器是否支持WebSocket, 主要此处要更换为自己的地址
+        if ('WebSocket' in window) {
+            websocket = new WebSocket("wss://www.bookstoremanager.top/wss/system/monitor");
+        } else {
+            alert('Not support websocket')
+        }
+
+        //连接发生错误的回调方法
+        websocket.onerror = function() {
+            console.log("error")
+        };
+
+        //连接成功建立的回调方法
+        websocket.onopen = function(event) {
+            console.log("open")
+            //setMessageInnerHTML("open");
+        }
+
+        //接收到消息的回调方法
+        websocket.onmessage = function(event) {
+            const parse = JSON.parse(event.data);
+            if(_.isEqual(data,parse)){
+               return;
+            }
+            const topSuccessRequest = parse['topSuccessRequest'] as any[];
+            let successXAxisData = []
+            let successSeriesData = []
+            for (const topSuccessRequestElement of topSuccessRequest) {
+                successXAxisData.push(topSuccessRequestElement['value'].split('.')[4])
+                successSeriesData.push(topSuccessRequestElement['score'])
+            }
+
+
+            // @ts-ignore
+            let successChart = echarts.init(document.getElementById('success'));
+            let successOption :EChartOption = {
+                title: {
+                    text: '成功请求TOP10',
+                    textAlign:'left'
+                },
+                tooltip: {
+                    trigger: 'axis',
+                    axisPointer: {
+                        type: 'shadow'
+                    }
+                },
+                xAxis: {
+                    type: 'category',
+                    data: successXAxisData,
+                    axisTick: {
+                        alignWithLabel: true
+                    },
+                    show:false
+                },
+                grid: {
+                    left: '3%',
+                    right: '4%',
+                    bottom: '3%',
+                    containLabel: false
+                },
+                yAxis: {
+                    type: 'value',
+                    show: false,
+                },
+                toolbox: {
+                    show: true,
+                    feature: {
+                        saveAsImage: {}
+                    }
+                },
+                series: [
+                    {
+                        name: '次数',
+                        data: successSeriesData,
+                        type: 'bar'
+                    }
+                ]
+            };
+            successChart.setOption(successOption);
+
+            const topFailRequest = parse['topFailRequest'] as any[];
+            let failXAxisData = []
+            let failSeriesData = []
+            for (const topSuccessRequestElement of topFailRequest) {
+                failXAxisData.push(topSuccessRequestElement['value'].split('.')[4])
+                failSeriesData.push(topSuccessRequestElement['score'])
+            }
+
+
+            // @ts-ignore
+            let failChart = echarts.init(document.getElementById('fail'));
+            let failOption :EChartOption = {
+                title: {
+                    text: '失败请求TOP10',
+                    textAlign:'left'
+                },
+                tooltip: {
+                    trigger: 'axis',
+                    axisPointer: {
+                        type: 'shadow'
+                    }
+                },
+                xAxis: {
+                    type: 'category',
+                    data: failXAxisData,
+                    axisTick: {
+                        alignWithLabel: true
+                    },
+                    show:false
+                },
+                grid: {
+                    left: '3%',
+                    right: '4%',
+                    bottom: '3%',
+                    containLabel: false
+                },
+                yAxis: {
+                    type: 'value',
+                    show: false,
+                },
+                toolbox: {
+                    show: true,
+                    feature: {
+                        saveAsImage: {}
+                    }
+                },
+                series: [
+                    {
+                        name: '次数',
+                        data: failSeriesData,
+                        type: 'bar'
+                    }
+                ]
+            };
+            failChart.setOption(failOption);
+
+            setData(parse)
+        }
+
+        //连接关闭的回调方法
+        websocket.onclose = function() {
+            console.log("close");
+        }
+
+        //监听窗口关闭事件，当窗口关闭时，主动去关闭websocket连接，防止连接还没断开就关闭窗口，server端会抛异常。
+        window.onbeforeunload = function() {
+            websocket.close();
+        }
+
+
+
+        //关闭连接
+        function closeWebSocket() {
+            if(!_.isEmpty(websocket)){
+                websocket.close();
+            }
+        }
+
+        //发送消息
+        function send() {
+            // websocket.send(message);
+        }
+    }
     /**styles 样式部分**/
 
     /**render**/
@@ -149,9 +243,9 @@ const Home:FC<HomeProps> = (props) => {
                     <Col span={12}>
                         <Card>
                             <Statistic
-                                title="历史加载类数"
-                                value={data.classLoadingMXBean && data.classLoadingMXBean.totalLoadedClassCount}
-                                 suffix={"个"}
+                                title="总线程数"
+                                value={data.totalThreadCount}
+                                suffix={"个"}
                                 valueStyle={{ color: '#3f8600' }}
                             />
                         </Card>
@@ -188,20 +282,6 @@ const Home:FC<HomeProps> = (props) => {
                             />
                         </Card>
                     </Col>
-                </Row>
-
-                <Row gutter={16}>
-                    <Col span={12}>
-                        <Card>
-                            <Statistic
-                                title="总进程数"
-                                value={data.totalThreadCount}
-                                suffix={"个"}
-                                valueStyle={{ color: '#3f8600' }}
-                            />
-                        </Card>
-                    </Col>
-
                 </Row>
 
             </div>
@@ -273,6 +353,11 @@ const Home:FC<HomeProps> = (props) => {
                         </Card>
                     </Col>
                 </Row>
+            </div>
+            <Divider>服务信息</Divider>
+            <div className="site-statistic-demo-card" style={{display:'inline-block'}}>
+                <div id={'success'} style={{flex: 1, height: '400px', width: '650px', display: 'inline-block', background: '#fff'}}/>
+                <div id={'fail'} style={{flex: 1, height: '400px', width: '650px', display: 'inline-block', background: '#fff'}}/>
             </div>
         </div>
 
